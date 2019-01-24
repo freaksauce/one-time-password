eef2d1251f829605e914cf8b551f867d2ceeb6dd
Jon <jon.bloomer@iag.com.au>
2019-01-25 10:32:25 +1100

    initial commit of firebase function

---
+++ functions/create_user.js
@@ -0,0 +1,14 @@
+const admin = require('firebase-admin');
+
+module.exports = function(req, res) {
+  // verify user provided phone number
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  // Format the phone number to remove dashes and parens
+
+  // Create a new user account using the phone number
+
+  // Respond to the user request, saying the account was made
+}
