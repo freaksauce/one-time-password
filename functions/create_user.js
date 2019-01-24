b0d1cb17b2d7e9b03acabe229c7aaa26d7f2062d
Jon <jon.bloomer@iag.com.au>
2019-01-25 10:44:40 +1100

    updated createUser function

--- functions/create_user.js
+++ functions/create_user.js
@@ -7,9 +7,11 @@ module.exports = function(req, res) {
   }
 
   // Format the phone number to remove dashes and parens
-  // const phone = 
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
 
-  // Create a new user account using the phone number
-
+  // Create a new user account using the phone number (phone number is assumed unique and used as the user id)
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
   // Respond to the user request, saying the account was made
 }
